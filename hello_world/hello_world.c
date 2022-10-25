c4be6e08d5d50ff4a4651bfdd62e0b6fc5d9b7cb
Lendl Uy <53631607+LendlUy@users.noreply.github.com>
2022-10-25 11:50:37 +0800

    Update hello_world.c
    Added more comments for ease of understanding

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,12 +1,16 @@
+// Author: Jan Lendl R. Uy
+// Title: Basic Hello world kernel module
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 
 // Meta Information
-MODULE_LICENSE("Deeply Human 365");
+MODULE_LICENSE("Proprietary");
 MODULE_AUTHOR("Lendl Uy");
 MODULE_DESCRIPTION("Basic Hello world kernel module");
 
+// Initialization
 static int __init hello_world_init(void){
 
     printk(KERN_ALERT "Hello, kernel\n");
@@ -14,6 +18,7 @@ static int __init hello_world_init(void){
 
 }
 
+// Clean-up/exit
 static void __exit hello_world_exit(void){
 
     printk(KERN_ALERT "Bye, kernel\n");
