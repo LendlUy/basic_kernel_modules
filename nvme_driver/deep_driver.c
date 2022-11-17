ca4d7c6ebede44d12547adef955de779e9d295d2
Lendl Uy <53631607+LendlUy@users.noreply.github.com>
2022-11-17 16:01:52 +0800

    Update deep_driver.c
    Added bus mastering and PCI MWI enabling, re-organized the code

--- nvme_driver/deep_driver.c
+++ nvme_driver/deep_driver.c
@@ -4,6 +4,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/pci.h>
+#include <linux/dma-mapping.h>
 #include <linux/pci_regs.h>
 
 // Meta Information
@@ -20,7 +21,7 @@ MODULE_DESCRIPTION("Driver for NVMe SSD control");
 #define VENDOR_ID 0x15b7
 #define DEVICE_ID 0x5003
 
-static struct pci_dev *ptr;
+static struct pci_dev *dev;
 
 /* Device ID and vendor ID for NVMe SSDs supported by Deep Driver
  * Format: PCI_DEVICE(vendor_id, device_id)
@@ -47,69 +48,32 @@ static struct pci_device_id ids[] = {
 	{ 0, }
 };
 
-static int read_config_space(struct pci_dev *ptr){
-
-  u8 int_line, int_pin, revision;
-	u16 vendor_id, device_id;
-	u32 bar[6];
-
-	pci_read_config_word(ptr, PCI_VENDOR_ID, &vendor_id);
-	printk(KERN_INFO "PCI_VENDOR_ID : 0x%X\n", vendor_id);
-
-	pci_read_config_word(ptr, PCI_DEVICE_ID, &device_id);
-	printk(KERN_INFO "PCI_DEVICE_ID : 0x%X\n", device_id);
-
-  pci_read_config_byte(ptr, PCI_REVISION_ID, &revision);
-	printk(KERN_INFO "PCI_REVISION_ID : 0x%d\n", revision);
-
-	pci_read_config_dword(ptr, PCI_BASE_ADDRESS_0, &bar[0]);
-	printk(KERN_INFO "BASE_ADDRESS_REGISTER_0 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_0, bar[0]);
-
-	pci_read_config_dword(ptr, PCI_BASE_ADDRESS_1, &bar[1]);
-	printk(KERN_INFO "BASE_ADDRESS_REGISTER_1 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_1, bar[1]);
-
-	pci_read_config_dword(ptr, PCI_BASE_ADDRESS_2, &bar[2]);
-	printk(KERN_INFO "BASE_ADDRESS_REGISTER_2 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_2, bar[2]);
-
-	pci_read_config_dword(ptr, PCI_BASE_ADDRESS_3, &bar[3]);
-	printk(KERN_INFO "BASE_ADDRESS_REGISTER_3 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_3, bar[3]);
-
-	pci_read_config_dword(ptr, PCI_BASE_ADDRESS_4, &bar[4]);
-	printk(KERN_INFO "BASE_ADDRESS_REGISTER_4 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_4, bar[4]);
-
-	pci_read_config_dword(ptr, PCI_BASE_ADDRESS_5, &bar[5]);
-	printk(KERN_INFO "BASE_ADDRESS_REGISTER_5 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_5, bar[5]);
-
-	pci_read_config_byte(ptr, PCI_INTERRUPT_LINE, &int_line);
-	printk(KERN_INFO "PCI_INTERRUPT_LINE : 0x%X \n", int_line);
-
-	pci_read_config_byte(ptr, PCI_INTERRUPT_PIN, &int_pin);
-	printk(KERN_INFO "PCI_INTERRUPT_PIN : 0x%X \n", int_pin);
-
-  return 0;
-}
-
-static int probe(struct pci_dev *ptr, const struct pci_device_id *id)   {
+static int probe(struct pci_dev *dev, const struct pci_device_id *id)   {
 
   printk("Inside the probe function\n");
 
   /* 
 	 * Check if a PCI device with specified vendor and device ID exists
    * Probe function will not be called if existing nvme driver is already installed
-	 * ptr is 0 if device exists, else it is a negative number
+	 * dev is 0 if device exists, else it is a negative number
 	*/
 
-  ptr = pci_get_device(VENDOR_ID, DEVICE_ID, ptr);
+  dev = pci_get_device(VENDOR_ID, DEVICE_ID, dev);
 
-	if (ptr == NULL){
-    printk("PCI device is not available\n");
+	if (dev == NULL){
+    printk("PCI device is not available!\n");
     return -1;
   }
 
 	return 0;
 }
 
-static void remove(struct pci_dev *ptr){
+static void remove(struct pci_dev *dev){
+
+  //pci_disable_device(dev);;
+  //pci_clear_master(dev);
+  //pci_clear_mwi(dev);
+
 	return;
 }
 
@@ -120,10 +84,170 @@ static struct pci_driver pci_driver = {
   .remove = remove,
 };
 
+int get_vendor_id(struct pci_dev *dev){
+
+	u16 vendor_id;
+  pci_read_config_word(dev, PCI_VENDOR_ID, &vendor_id);
+
+	return vendor_id;
+}
+
+int get_device_id(struct pci_dev *dev){
+
+	u16 device_id;
+  pci_read_config_word(dev, PCI_DEVICE_ID, &device_id);
+
+	return device_id;
+}
+
+int get_revision_id(struct pci_dev *dev){
+
+	u8 revision;
+  pci_read_config_byte(dev, PCI_REVISION_ID, &revision);
+
+	return revision;
+}
+
+int get_pci_bar0(struct pci_dev *dev){
+
+	u8 bar;
+  pci_read_config_byte(dev, PCI_BASE_ADDRESS_0, &bar);
+
+	return bar;
+}
+
+int get_pci_bar1(struct pci_dev *dev){
+
+	u8 bar;
+  pci_read_config_byte(dev, PCI_BASE_ADDRESS_1, &bar);
+
+	return bar;
+}
+
+int get_pci_bar2(struct pci_dev *dev){
+
+	u8 bar;
+  pci_read_config_byte(dev, PCI_BASE_ADDRESS_2, &bar);
+
+	return bar;
+}
+
+int get_pci_bar3(struct pci_dev *dev){
+
+	u8 bar;
+  pci_read_config_byte(dev, PCI_BASE_ADDRESS_3, &bar);
+
+	return bar;
+}
+
+int get_pci_bar4(struct pci_dev *dev){
+
+	u8 bar;
+  pci_read_config_byte(dev, PCI_BASE_ADDRESS_4, &bar);
+
+	return bar;
+}
+
+int get_pci_bar5(struct pci_dev *dev){
+
+	u8 bar;
+  pci_read_config_byte(dev, PCI_BASE_ADDRESS_5, &bar);
+
+	return bar;
+}
+
+int get_int_line(struct pci_dev *dev){
+
+	u8 int_line;
+  pci_read_config_byte(dev, PCI_INTERRUPT_LINE, &int_line);
+
+	return int_line;
+}
+
+int get_int_pin(struct pci_dev *dev){
+
+	u8 int_pin;
+  pci_read_config_byte(dev, PCI_INTERRUPT_PIN, &int_pin);
+
+	return int_pin;
+}
+
+/* 
+	* Reads the PCI configuration space of the device
+	*/
+
+static void read_config_space(struct pci_dev *dev){
+
+  u8 int_line, int_pin, revision_id;
+	u16 vendor, device_id;
+	u8 bar;
+
+	vendor = get_vendor_id(dev);
+	printk(KERN_INFO "PCI_VENDOR_ID : 0x%X\n", vendor);
+
+	device_id = get_device_id(dev);
+	printk(KERN_INFO "PCI_DEVICE_ID : 0x%X\n", device_id);
+
+  revision_id = get_revision_id(dev);
+	printk(KERN_INFO "PCI_REVISION_ID : 0x%d\n", revision_id);
+
+	bar = get_pci_bar0(dev);
+	printk(KERN_INFO "BASE_ADDRESS_REGISTER_0 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_0, bar);
+
+	bar = get_pci_bar1(dev);
+	printk(KERN_INFO "BASE_ADDRESS_REGISTER_1 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_1, bar);
+
+	bar = get_pci_bar2(dev);
+	printk(KERN_INFO "BASE_ADDRESS_REGISTER_2 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_2, bar);
+
+	bar = get_pci_bar3(dev);
+	printk(KERN_INFO "BASE_ADDRESS_REGISTER_3 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_3, bar);
+
+	bar = get_pci_bar4(dev);
+	printk(KERN_INFO "BASE_ADDRESS_REGISTER_4 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_4, bar);
+
+	bar = get_pci_bar5(dev);
+	printk(KERN_INFO "BASE_ADDRESS_REGISTER_5 - 0x%.8X, VAL : 0x%X\n", PCI_BASE_ADDRESS_5, bar);
+
+	int_line = get_int_line(dev);
+	printk(KERN_INFO "PCI_INTERRUPT_LINE : 0x%X \n", int_line);
+
+	int_pin = get_int_pin(dev);
+	printk(KERN_INFO "PCI_INTERRUPT_PIN : 0x%X \n", int_pin);
+}
+
+static int enable_nvme(struct pci_dev *dev) {
+
+  /*
+   * Enables the NVMe device connected to the machine.
+   * Checks first if it is a PCIe device before enabling.
+   */
+
+  bool is_pcie;
+  int ret;
+
+  is_pcie = pci_is_pcie(dev);
+  if (!is_pcie) {
+    printk("Device is not a PCIe device! It will not be enabled.\n");
+    return -1;
+  }
+
+  ret = pci_enable_device(dev);
+  if (ret) {
+    printk("PCI device was not enabled!\n");
+    return -1;
+  }
+
+  printk("PCI device was successfully enabled.\n");
+
+  return 0;
+}
+
 // Driver initialization
-static int __init nvme_init(void){
+static int __init init_nvme(void){
 
   int ret;
+  unsigned long res_start, res_end, res_flags;
 
   if (pci_register_driver(&pci_driver) != 0) {
     printk("Driver was not registered!\n");
@@ -132,37 +256,52 @@ static int __init nvme_init(void){
 
   printk("Driver was successfully registered!\n");
 
-  ptr = pci_get_device(VENDOR_ID, DEVICE_ID, ptr);
+  dev = pci_get_device(VENDOR_ID, DEVICE_ID, dev);
 
-  read_config_space(ptr);
-
-  ret = pci_enable_device(ptr);
-  if (ret) {
-    printk("PCI device was not enabled!\n");
+  ret = enable_nvme(dev);
+  if (ret != 0) {
     return -1;
   }
 
-  printk("PCI device was successfully enabled!\n");
+  //read_config_space(dev);
 
-  pci_set_master(ptr); // Enables DMA
-  pci_set_mwi(ptr); // Enables PCI Memory-Write-Invalidate
+  pci_set_master(dev); // Sets the bus to master for the device
+  printk("Bus master has been set for the device.\n");
 
-  ret = pci_request_region(ptr, 0x0, "nvme_bar_0"); // Requests a memory region to be used by nvme
+  ret = pci_set_mwi(dev); // Enables PCI Memory-Write-Invalidate transaction
   if (ret != 0) {
     printk("Requested region was not allocated!\n");
     return -1;
   }
+  printk("PCI MWI transactions have been enabled.\n");
 
-  return 0;
+  /*
+   * Gets resource information from a specific BAR
+   */
+  
+  res_start = pci_resource_start(dev, 0);
+  printk("resource start of bar 0: 0x%X\n", res_start);
+
+  res_end = pci_resource_end(dev, 0);
+  printk("resource end of bar 0: 0x%X\n", res_end);
+
+  res_flags = pci_resource_flags(dev, 0);
+  printk("resource flags of bar 0: 0x%X\n", res_flags);
+
+	return 0;
+  
 }
 
-// Clean-up/exit
-static void __exit nvme_exit(void){
+/*
+ * Exit code for the driver
+ */
+
+static void __exit exit_nvme(void){
 
   pci_unregister_driver(&pci_driver);
   printk("Driver unregistered from the machine\n");
 
 }
 
-module_init(nvme_init);
-module_exit(nvme_exit);
+module_init(init_nvme);
+module_exit(exit_nvme);
